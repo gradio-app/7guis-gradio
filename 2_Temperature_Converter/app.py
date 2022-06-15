4f084b02b735406cb1dbe32245dada4ec061772d
Mashrur Morshed <38450043+ID56@users.noreply.github.com>
2022-06-15 15:52:03 +0600

    Submit-based. (#1)

--- 2_Temperature_Converter/app.py
+++ 2_Temperature_Converter/app.py
@@ -3,21 +3,16 @@ import gradio as gr
 with gr.Blocks() as demo:
     gr.Markdown(
         """# Temperature Converter
-        Submit the temperature in either Celsius or Fahrenheit to get the other!
+        Update the temperature in either Celsius or Fahrenheit and press enter to get the other!
         """
     )
 
     with gr.Row():
-        with gr.Box():
-            celsius = gr.Number(0, label="Celsius")
-            c2f = gr.Button("Submit")
+        celsius = gr.Number(0, label="Celsius")
+        fahrenheit = gr.Number(32, label="Fahrenheit")
 
-        with gr.Box():
-            fahrenheit = gr.Number(32, label="Fahrenheit")
-            f2c = gr.Button("Submit")
-
-    c2f.click(lambda c: c * 9 / 5 + 32, celsius, fahrenheit)
-    f2c.click(lambda f: (f - 32) * 5 / 9, fahrenheit, celsius)
+        celsius.submit(lambda c: c * 9 / 5 + 32, celsius, fahrenheit)
+        fahrenheit.submit(lambda f: (f - 32) * 5 / 9, fahrenheit, celsius)
 
 if __name__ == "__main__":
     demo.launch()

4664cecf7397232be2e634230ecee2f09d7b6095
kysmet <kysmet@Goolan.local>
2022-06-16 22:04:40 +0530

    implemented prefix filter

--- 5_CRUD/app.py
+++ 5_CRUD/app.py
@@ -6,7 +6,7 @@ dummy_data = [["Alice", "Springs"], ["Max", "Brown"]]
 
 def fetch_data():
     """Fetch data from the data store. 
-    In this case it's just an numpy array
+    In this case it's just an array.
     NOTE: This should happen at page load."""
     return dummy_data
 
@@ -15,10 +15,8 @@ def create_item(name, surname):
     dummy_data.append([name, surname])
     return dummy_data
 
-def update_item(idx, name, surname):
+def update_item(out):
     """Update an item in the data store"""
-    idx = dummy_data.index([name, surname])
-    dummy_data.insert(idx,)
     return dummy_data
 
 def delete_item(name, surname):
@@ -26,10 +24,14 @@ def delete_item(name, surname):
     dummy_data.remove([name, surname])
     return dummy_data
 
-def filter_prefix(prefix):
+def filter_with_prefix(prefix):
     """Return all the item whoes surnames starting with prefix
     else return an empty list"""
-    return [x for x in dummy_data[:,1] if re.search(f"^{prefix}", x)]
+    filtered_data = []
+    for idx, lst in enumerate(dummy_data):
+        if re.search(f"^{prefix}", lst[1]):
+            filtered_data.append(dummy_data[idx])
+    return filtered_data
 
 with gr.Blocks() as demo:
 
@@ -53,8 +55,9 @@ with gr.Blocks() as demo:
         delete_btn = gr.Button("Delete")
     
     create_btn.click(create_item, [name, surname], out)
-    update_btn.click(update_item, [name, surname], out)
+    update_btn.click(update_item, out, out)
     delete_btn.click(delete_item, [name, surname], out)
+    filter_prefix.change(filter_with_prefix, filter_prefix, out)
 
 if __name__ == "__main__":
     demo.launch()

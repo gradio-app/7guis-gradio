6722cd2ec047f320a1c7c798a70d7f20caeb612f
ID56 <mashrurmahmud@iut-dhaka.edu>
2022-06-18 01:40:28 +0600

    Flight-booker based on updating interactivity and visibility.

--- 3_Flight_Booker/app.py
+++ 3_Flight_Booker/app.py
@@ -1,63 +1,62 @@
 import gradio as gr
 from datetime import datetime
 
+def return_state(flight_type: str):
+    """Changes interactivity of the returning date Textbox depending on flight type."""
+    state = flight_type == "return flight"
+    return gr.Textbox.update(value="", interactive=state)
 
-def update_return_visibility(flight_type: str):
-    """Changes visibility of the returning date, depending on flight type."""
-    if flight_type == "return flight":
-        visible = True
-    else:
-        visible = False
-    return gr.Textbox.update(visible=visible)
-
-
-def on_click(flight_type: str, start_date: str, return_date: str):
-    """If Book button is clicked displays message."""
+def display_message(flight_type: str, start_date: str, return_date: str):
+    """If the book button is clicked, displays a message."""
     if flight_type == "one-way flight":
-        message = f"You have booked a {flight_type} flight on {start_date}"
+        message = f"You have booked a {flight_type} on {start_date}."
     else:
-        message = f"You have booked a {flight_type} flight starting on {start_date} and returning on {return_date}"
-    return gr.TextArea.update(value=message)
+        message = f"You have booked a {flight_type} starting on {start_date} and returning on {return_date}."
+    return gr.TextArea.update(visible=True, value=message)
 
+def remove_message():
+    """Clears and removes message display."""
+    return gr.TextArea.update(visible=False, value="")
 
-def update_date(flight_type: str, start_date: str, return_date: str):
-    """Changing the date values. Any change to dates also clear out the message."""
-    button_visible = True
+def check_date(flight_type: str, start_date: str, return_date: str):
+    """If the date is updated, checks format and updates button."""
+    state = True
     try:
         d1 = datetime.strptime(start_date, "%d.%m.%Y")
         if flight_type == "return flight":
             d2 = datetime.strptime(return_date, "%d.%m.%Y")
             if d1 > d2: 
-                button_visible = False
-    except Exception: # Exception is raised by datetime.strptime when input format is wrong
-        button_visible = False
-
-    return gr.Button.update(visible=button_visible), gr.TextArea.update(value="")
-
-
-################################
-# main demo
-################################
+                state = False
+    # When date input format is wrong
+    except Exception:
+        state = False
+    return gr.Button.update(visible=state)
 
 TODAY = datetime.now().strftime("%d.%m.%Y")
 
 with gr.Blocks() as demo:
     gr.Markdown(
-        """# Flight Booking
+        """# Flight Booker
         """
     )
     with gr.Column():
-        flight_type = gr.Dropdown(["one-way flight", "return flight"], value="return flight", show_label=False)
+        flight_type = gr.Dropdown(["one-way flight", "return flight"], value="one-way flight", show_label=False)
         start_date = gr.Textbox(TODAY, max_lines=1, placeholder="dd.mm.yyyy", show_label=False)
-        return_date = gr.Textbox(TODAY, max_lines=1, placeholder="dd.mm.yyyy", show_label=False) # supposed to be turned off / not visible initially, but keeping it on for now
+        return_date = gr.Textbox("", max_lines=1, placeholder="dd.mm.yyyy", show_label=False, interactive=False)
         button = gr.Button("Book")
-        message = gr.Textbox(max_lines=1, visible=True, show_label=False)
+        message = gr.Textbox(max_lines=1, visible=False, interactive=False, show_label=False)
 
-        flight_type.change(update_return_visibility, flight_type, return_date)
-        start_date.change(update_date, [flight_type, start_date, return_date], [button, message])
-        return_date.change(update_date, [flight_type, start_date, return_date], [button, message])
-        button.click(on_click, [flight_type, start_date, return_date], message)
+        flight_type.change(return_state, flight_type, return_date)
+        flight_type.change(check_date, [flight_type, start_date, return_date], button)
+        flight_type.change(remove_message, [], message)
 
+        start_date.change(check_date, [flight_type, start_date, return_date], button)
+        start_date.change(remove_message, [], message)
+
+        return_date.change(check_date, [flight_type, start_date, return_date], button)
+        return_date.change(remove_message, [], message)
+
+        button.click(display_message, [flight_type, start_date, return_date], message)
 
 if __name__ == "__main__":
     demo.launch()
